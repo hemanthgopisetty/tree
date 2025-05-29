ddc09c3b7d987e85b40df1a31785e3a67c4af4a2
hemanthgopisetty <hemanth.508136@gmail.com>
2025-05-30 00:18:35 +0530

    Tree Inorder Traversal

--- Inorder.cpp
+++ Inorder.cpp
@@ -24,7 +24,7 @@ class Solution {
             return ;
         }
         inorder(root->left);
-        cout<<root->val;
+        cout<<root->val<<" -> ";
         inorder(root->right);
     }
 };
@@ -35,8 +35,26 @@ int main(){
     root->right = new TreeNode(3);
     root->left->left = new TreeNode(4);
     root->left->right = new TreeNode(5);
-    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(6);
     Solution sol;
     sol.inorder(root);
     return 0;
 }
+
+/**
+ *                (1)
+ *               /   \
+ *             (2)    (3)
+ *            /   \     \
+ *           (4)  (5)    (6)
+ * 
+ * find the inorder traversal for the give binary tree
+ *  4->2->5->1->3->
+ * 
+ * start
+ * if(!root) end
+ * left subtree 
+ * root
+ * right subtree
+ * repeat
+ */
